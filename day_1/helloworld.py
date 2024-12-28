3ff72ec56327fbb0d4f09d5892c32692c66185a3
smasheroonie <68129402+Smasheroonie@users.noreply.github.com>
2024-12-28 22:57:00 +0000

    initial commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,45 @@
+# Day 1 of Python!
+
+import math
+
+# Basic operations
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3**4)
+print(3//4)
+
+print('Luke')
+print('Goncalves')
+print('United Kingdom')
+print('I am enjoying 30 days of python')
+
+# Typing
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Luke'))
+print(type('Goncalves'))
+print(type('United Kingdom'))
+
+# Type examples
+
+my_int = 24
+my_float = 24.4
+my_complex = 24j
+my_string = 'Hello world'
+my_bool = True
+my_list = ['Hello', 'World', 123]
+my_tuple = (1, 2, 3, 'Hello')
+my_set = {1, 2, 3, 'Hello'}
+my_dict = {'greeting': 'Hello world', 'numbers': 123}
+
+# Euclidian distance function
+
+print(math.sqrt(abs(2-10)**2 + abs(3-8)**2))
